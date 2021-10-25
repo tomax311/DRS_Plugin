698b5d3101b4d2b33117f855cd92f51da624014f
tomax311 <matteo.limauge@gmail.com>
2021-10-25 19:57:12 +0200

    Startup default

--- src/main/java/tomax311/drs_plugin/DRS_Plugin.java
+++ src/main/java/tomax311/drs_plugin/DRS_Plugin.java
@@ -1,17 +1,21 @@
 package tomax311.drs_plugin;
 
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class DRS_Plugin extends JavaPlugin {
 
+    public static DRS_Plugin plugin;
+
     @Override
     public void onEnable() {
-        // Plugin startup logic
+        plugin = this;
+        saveDefaultConfig();
+
+        System.out.println("DRS_Plugin is on");
+
+
 
     }
 
-    @Override
-    public void onDisable() {
-        // Plugin shutdown logic
-    }
 }
