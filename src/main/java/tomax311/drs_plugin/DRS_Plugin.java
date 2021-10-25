8ff104fd34951c61cc931ddc7094198660149e77
Matteo Limauge <matteo.limauge@gmail.com>
2021-10-25 19:47:14 +0200

    The-default-environement

---
+++ src/main/java/tomax311/drs_plugin/DRS_Plugin.java
@@ -0,0 +1,17 @@
+package tomax311.drs_plugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class DRS_Plugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
